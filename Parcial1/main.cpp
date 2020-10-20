56178c6c18ec686b5a5a1fc346ad8b2036ffe538
Sebastianuribe1212 <sebastian.uribea@udea.edu.co>
2020-10-20 13:24:10 -0500

    Finalizado la función que dispara el cañon defensivo, comenzamos a hacer la colision de las balas

--- Parcial1/main.cpp
+++ Parcial1/main.cpp
@@ -1,3 +1,8 @@
+/*
+ *Sebastián Uribe Álvarez-1000438232
+ *
+ *
+ */
 #include <iostream>
 #include <cmath>
 using namespace std;
@@ -6,91 +11,96 @@ using namespace std;
 
 
 
-//entra la distancia y el angulo de disparo.
+//entra la distancia y el angulo de disparo, la altura del cañon ofensivo y defensivo
 //retorna velocidad en 'x' y 'y' y y tiempo en el que llega al cañon defensivo.
-int disparoO (int, int);
-// hallar la posicion del disparo ofensivo para disparar el defensivo
-void posdisparo(float, float, float, float);
+int disparoO (int, int, int, int);
+
+//entra la distancia y el angulo de disparo, la altura del cañon ofensivo y defensivo
+//retorna velocidad en 'x' y 'y' y y tiempo en el que llega al cañon ofensivo.
+int disparoD (int, int, int, int);
+
+//entra, tiempo, velocidad y angulo del disparo ofensivo,
+//sale, velocidad y angulo para impactar en el disparo ofensivo.
+int disparodefensivo(int, int, int);
+
 
 float *angulo1P, *angulo2P, *angulo3P;
 float *xP, *yP, *anguloD;
-int *vOxp, *vOyp;
+int *vOxp, *vOyp,*vDxp, *vDyp;
 int main()
 {
-    // posicion de los cañones
-   // float posOx, posOy, posDx, posDy;
+    int Ho, Hd;
+
     float angulo1,angulo2, angulo3;
     int vOx1, vOy1,  vOx2, vOy2, vOx3, vOy3;
-    int t1 , t2 , t3;
-    // rangos de tolerancia para loscañones
-    //float rangod = 0.025, rangoO = 0.05;
+    int tO1 , tO2 , tO3;
+
+    int vDx1, vDy1,  vDx2, vDy2, vDx3, vDy3;
+    int tD1 , tD2 , tD3;
+
 
     int d;
 
     cout<<"Ingrese la distancia entre los canones: ";
     cin >> d;
 
-    // posiciones de los cañones;
-    /*
-    cout << "Ingrese la posicion del cañon ofensivo en la coordenada X: ";
-    cin >> posOx;
+    cout<<"Ingrese altura del canon ofensivo: ";
+    cin >> Ho;
 
-    cout << "Ingrese la posicion del cañon ofensivo en la coordenada Y: ";
-    cin >> posOy;
-
-    cout << "Ingrese la posicion del cañon Defensivo en la coordenada X: ";
-    cin >> posDx;
-
-    cout << "Ingrese la posicion del cañon Defensivo en la coordenada Y: ";
-    cin >> posDy;
-    */
+    cout<<"Ingrese la altura del canon defensivo: ";
+    cin >> Hd;
 
 
-    //hallar tiros para darle al defensivo.
-
-    // disp1
-    // por temas de tiempo voy a ingresar tres velocidades y angulos aleatorios y ver si colisionan
+    // tiros ofensivos para dañar el cañon defensivo.
+   //hallar tiros para darle al defensivo.
+  //angulos aleatorios y ver si colisionan
     angulo1 = 45;
-    t1 =  disparoO (d, angulo1);
+    tO1 =  disparoO (d, angulo1, Ho, Hd);
     vOx1 =  *vOxp;
     vOy1 = *vOyp;
     angulo2 = 40;
-    t2 = disparoO(d, angulo2 );
+    tO2 = disparoO(d, angulo2, Ho, Hd );
     vOx2 =  *vOxp;
     vOy2 = *vOyp;
     angulo3 = 35;
-    t3 = disparoO(d, angulo3);
+    tO3 = disparoO(d, angulo3, Ho, Hd);
     vOx3 =  *vOxp;
     vOy3 = *vOyp;
 
-    cout << "t1: "<<t1<<"vOx1: "<<vOx1<<"vOy1: "<<vOy1<<endl;
-    cout << "t2: "<<t2<<"vOx2: "<<vOx2<<"vOy2: "<<vOy2<<endl;
-    cout << "t3: "<<t3<<"vOx3: "<<vOx3<<"vOy3: "<<vOy3<<endl;
+    cout << "1. (angulo 45) tiempo: "<<tO1<<" velocidad en x: "<<vOx1<<" velocidad en y: "<<vOy1<<endl;
+    cout << "2. (angulo 40) tiempo: "<<tO2<<" velocidad en x: "<<vOx2<<" velocidad en y: "<<vOy2<<endl;
+    cout << "3. (angulo 35) tiempo: "<<tO3<<" velocidad en x: "<<vOx3<<" velocidad en y: "<<vOy3<<endl;
+    cout << "si la salida es 0 es porque no colisiona."<<endl;
 
 
+    cout <<" "<<endl;
+    cout <<"punto2 tiros defensivos que golpean al canon defensivo"<<endl;
+   angulo1 = 45;
+    tD1 =  disparoD (d, angulo1, Ho, Hd);
+    vDx1 =  *vDxp;
+    vDy1 = *vDyp;
+    angulo2 = 40;
+    tD2 = disparoD(d, angulo2, Ho, Hd );
+    vDx2 =  *vDxp;
+    vDy2 = *vDyp;
+    angulo3 = 35;
+    tD3 = disparoD(d, angulo3, Ho, Hd);
+    vDx3 =  *vDxp;
+    vDy3 = *vDyp;
+
+    cout << "1. (angulo 45) tiempo: "<<tD1<<" velocidad en x: "<<vDx1<<" velocidad en y: "<<vDy1<<endl;
+    cout << "2. (angulo 40) tiempo: "<<tD2<<" velocidad en x: "<<vDx2<<" velocidad en y: "<<vDy2<<endl;
+    cout << "3. (angulo 35) tiempo: "<<tD3<<" velocidad en x: "<<vDx3<<" velocidad en y: "<<vDy3<<endl;
+    cout << "si la salida es 0 es porque no colisiona."<<endl;
 
 
     return 0;
 }
 
-/*
-void posdisparo(float angulo, float v, float posx, float posy){
-    float x, y, anguloD;
 
-
-
-    x = posx + v*2.5;
-    y = posy + v*2.5-(4.9*6.25);
-
-    cout <<": x="<<x<<" y= "<<y<<endl;
-
-
-}
-
-*/
-//entra la distancia y el angulo de disparo.
+ //entra la distancia y el angulo de disparo.
 //retorna velocidad en 'x' y 'y' y y tiempo en el que llega al cañon defensivo.
-int disparoO (int distancia, int angulo){
+int disparoO (int distancia, int angulo, int Ho, int Hd){
     float posOx, posOy;
     float pi = 3.1416, g = 9.81, dO = 0.05*distancia ;
     float aO = angulo * pi/180;
@@ -102,8 +112,8 @@ int disparoO (int distancia, int angulo){
         vOy =vO*sin(aO);
         for (int t = 0; t<=100; t++){//
             posOx = vOx*t;
-            posOy =( 0 + vOy*t) - (0.5*g*(t*t));
-            if(sqrt(pow(distancia-posOx,2)+pow((-100)-posOy,2))<=dO){ // defensivo en x = d defensivo en y = h (altura)
+            posOy =( Ho + vOy*t) - (0.5*g*(t*t));
+            if(sqrt(pow(distancia-posOx,2)+pow(Hd-posOy,2))<=dO){ // defensivo en x = d defensivo en y = h (altura)
                  tiempo = t;
                  vOxp = &vOx;
                  vOyp = &vOy;
@@ -121,8 +131,38 @@ int disparoO (int distancia, int angulo){
    return tiempo;
 }
 
+int disparoD (int distancia, int angulo, int Ho, int Hd){
+    float posOx, posOy;
+    float pi = 3.1416, g = 9.81, dO = 0.05*distancia ;
+    float aO = angulo * pi/180;
+    int  vOx, vOy, cero =0;
+    int tiempo=0;
+    bool flag = false;
+    for(int vO = 0; vO<=100; vO+=5){
+        vOx =vO*cos(aO);
+        vOy =vO*sin(aO);
+        for (int t = 0; t<=100; t++){//
+            posOx = vOx*t;
+            posOy =( Hd + vOy*t) - (0.5*g*(t*t));
+            if(sqrt(pow(distancia-posOx,2)+pow(Ho-posOy,2))<=dO){ // defensivo en x = d defensivo en y = h (altura)
+                 tiempo = t;
+                 vDxp = &vOx;
+                 vDyp = &vOy;
+                 flag = true;
+                 return tiempo;
+                 break;
+            }
+        }
+    }
+   if (flag == false){
+       tiempo = 0;
+       vDxp = &cero;
+       vDyp = &cero;
+   }
+   return tiempo;
+}
 
-
+int disparodefensivo(int, int, int);
 
 
 
